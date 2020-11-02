ab93a6318868ff5062ba0ab383b82f734cfe6a58
K Annabel Bligh <kabligh@gmail.com>
2020-11-02 15:44:21 +0000

    Challenge 6 – enforcing maximum balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,12 +2,15 @@ class Oystercard
 
   attr_reader :balance
 
+  MAX_BALANCE = 90
+
   def initialize(balance = 0)
     @balance = balance
   end
 
   def top_up(amount)
-    @balance += amount 
-  end 
+    raise StandardError.new "You cannot top up over the limit of £#{MAX_BALANCE} " if @balance + amount > MAX_BALANCE
+    @balance += amount
+  end
 
-end 
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,12 +6,20 @@ describe Oystercard do
     expect(subject.balance).to eq 0
   end
 
-  it "can be topped up with money" do 
-    expect(subject).to respond_to(:top_up).with(1).argument 
+  it "can be topped up with money" do
+    expect(subject).to respond_to(:top_up).with(1).argument
   end
 
-  it "balance increases by 5 when topped up by 5" do 
+  it "balance increases by 5 when topped up by 5" do
     expect { subject.top_up(5) }.to change { subject.balance }.by(5)
+  end
+
+  it "has a maximum balance constant set at 90" do
+    expect(Oystercard::MAX_BALANCE).to eq(90)
   end 
-    
-end 
+
+  it "Should raise an error message if topped up above maximum balance of 90" do
+    expect { subject.top_up(91) }.to raise_error(StandardError)
+  end
+
+end
