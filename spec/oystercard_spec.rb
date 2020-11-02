104854a7772db281a40418771324d2b38649591e
K Annabel Bligh <kabligh@gmail.com>
2020-11-02 15:10:15 +0000

    Challenge 5 – enabled top up functionality

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,8 +2,16 @@ require 'oystercard'
 
 describe Oystercard do
 
-  it 'initialize' do
+  it "has a default balance of 0 when set up" do
     expect(subject.balance).to eq 0
   end
+
+  it "can be topped up with money" do 
+    expect(subject).to respond_to(:top_up).with(1).argument 
+  end
+
+  it "balance increases by 5 when topped up by 5" do 
+    expect { subject.top_up(5) }.to change { subject.balance }.by(5)
+  end 
     
 end 
